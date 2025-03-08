302da407be1b37449c97c8530cd3f4058fe804da
Bas Smit <bas.smit@live.nl>
2025-03-08 20:07:54 +0100

    vercel v3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.libelnet.nl", "www.bassmit.dev"],
+    domains: [
+      "www.libelnet.nl",
+      "www.bassmit.dev",
+      "https://werkenbijlibelnet.nl/",
+    ],
   },
 };
 
